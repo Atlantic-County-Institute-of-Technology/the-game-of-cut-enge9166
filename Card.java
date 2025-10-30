798736197b246900c7b509a25493b00bfe887bab
enge9166 <116181104+enge9166@users.noreply.github.com>
2025-10-30 12:40:59 -0400

    h

--- Card.java
+++ Card.java
@@ -13,7 +13,7 @@ public class Card
     private final int face;
     private final int suit;
     private final String[] faces = {null,"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"}
-    private final String[] Suits = {null, "Spades","Hearts","Clubs","Diamonds"}
+    private final String[] suits = {null, "Spades","Hearts","Clubs","Diamonds"}
 
 
 
@@ -34,6 +34,7 @@ public class Card
     public String getSuit(){
         return suit;
     }
+    public String toString(){return faces[face] + " of " + suits[suit]; }
 
 
 }
